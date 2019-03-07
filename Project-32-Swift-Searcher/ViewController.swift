193a22ce651f9f65187e7e494ace37352adf9bf2
David Verbenyi <dverbenyi@gmail.com>
2019-03-07 13:25:52 +0000

    Change projects array from 2d String array into array of tuples (title: String, subTitle: String)

--- Project-32-Swift-Searcher/ViewController.swift
+++ Project-32-Swift-Searcher/ViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class ViewController: UITableViewController {
     
-    var projects = [[String]]()
+//    var projects = [[String]]()
+    var projects = [(title: String, subTitle: String)]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,14 +20,14 @@ class ViewController: UITableViewController {
     }
     
     private func addProjects() {
-        projects.append(["Project 1: Storm Viewer", "Constants and variables, UITableView, UIImageView, FileManager, storyboards"])
-            projects.append(["Project 2: Guess the Flag", "@2x and @3x images, asset catalogs, integers, doubles, floats, operators (+= and -=), UIButton, enums, CALayer, UIColor, random numbers, actions, string interpolation, UIAlertController"])
-            projects.append(["Project 3: Social Media", "UIBarButtonItem, UIActivityViewController, the Social framework, URL"])
-            projects.append(["Project 4: Easy Browser", "loadView(), WKWebView, delegation, classes and structs, URLRequest, UIToolbar, UIProgressView., key-value observing"])
-            projects.append(["Project 5: Word Scramble", "Closures, method return values, booleans, NSRange"])
-            projects.append(["Project 6: Auto Layout", "Get to grips with Auto Layout using practical examples and code"])
-            projects.append(["Project 7: Whitehouse Petitions", "JSON, Data, UITabBarController"])
-            projects.append(["Project 8: 7 Swifty Words", "addTarget(), enumerated(), count, index(of:), property observers, range operators."])
+        projects.append(("Project 1: Storm Viewer", "Constants and variables, UITableView, UIImageView, FileManager, storyboards"))
+            projects.append(("Project 2: Guess the Flag", "@2x and @3x images, asset catalogs, integers, doubles, floats, operators (+= and -=), UIButton, enums, CALayer, UIColor, random numbers, actions, string interpolation, UIAlertController"))
+            projects.append(("Project 3: Social Media", "UIBarButtonItem, UIActivityViewController, the Social framework, URL"))
+            projects.append(("Project 4: Easy Browser", "loadView(), WKWebView, delegation, classes and structs, URLRequest, UIToolbar, UIProgressView., key-value observing"))
+            projects.append(("Project 5: Word Scramble", "Closures, method return values, booleans, NSRange"))
+            projects.append(("Project 6: Auto Layout", "Get to grips with Auto Layout using practical examples and code"))
+            projects.append(("Project 7: Whitehouse Petitions", "JSON, Data, UITabBarController"))
+            projects.append(("Project 8: 7 Swifty Words", "addTarget(), enumerated(), count, index(of:), property observers, range operators."))
     }
     
     private func makeAttributedString(title: String, subTitle: String) -> NSAttributedString {
@@ -50,7 +51,7 @@ class ViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.attributedText = makeAttributedString(title: projects[indexPath.row][0], subTitle: projects[indexPath.row][1])
+        cell.textLabel?.attributedText = makeAttributedString(title: projects[indexPath.row].title, subTitle: projects[indexPath.row].subTitle)
         
         return cell
     }
